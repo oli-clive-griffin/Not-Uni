5ce7eadad112e7d3987c6ff92e4fda513ff05cdf
corian-youdale <corian.youdale@hotmail.com>
2021-03-23 12:57:35 +1300

    update ssl rejection on production in both knexfiles

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -39,7 +39,8 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
     pool: { 
       min: 2, 
       max: 10 
